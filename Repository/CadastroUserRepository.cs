194cbdda0aecaccb489f3ac2effad9e8999685a8
Gustavo Ryan Joaquina <gugagg002@gmail.com>
2024-01-18 20:59:47 -0300

    CadastroUserRepository

--- Repository/CadastroUserRepository.cs
+++ Repository/CadastroUserRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace PI_Entra21_Back_end.Repository
+﻿using Mysqlx.Prepare;
+using PI_Entra21_Back_end.Contracts.Repository;
+using PI_Entra21_Back_end.DTO;
+
+namespace PI_Entra21_Back_end.Repository
 {
-    public class CadastroUserRepository
+    public class CadastroUserRepository : Connection, ICadastroUserRepository
     {
+        public async Task Add(CadastroUserDTO user)
+        {
+            string sql = @"
+                INSERT INTO USER(Name, Age, Phone, Cep, Email, Password)
+                    VALUE (@Name, @Age, @Phone, @Cep, @Email, @Password)";
+            await Execute(sql, user);
+        }
     }
 }
