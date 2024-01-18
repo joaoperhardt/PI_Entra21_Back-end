d9124caed7c048d0515694505d15a87cd13af71d
Gustavo Ryan Joaquina <gugagg002@gmail.com>
2024-01-18 19:19:04 -0300

    first commit

---
+++ DTO/CadastroUserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace PI_Entra21_Back_end.DTO
+{
+    public class CadastroUserDTO
+    {
+        public string Name { get; set; }
+        public string Data { get; set; }
+        public string Cep { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Entity/CadastroUserEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace PI_Entra21_Back_end.Entity
+{
+    public class CadastroUserEntity
+    {
+		public int? Id { get; set; }
+		public string Name { get; set; }
+		public string Data { get; set; }
+		public string Cep { get; set; }
+		public string PhoneNumber { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+
+	}
+}
