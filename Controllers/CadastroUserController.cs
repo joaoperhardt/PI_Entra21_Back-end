3f7b21f6a117c3aaf57801763991cce3e14da1a1
Gustavo Ryan Joaquina <gugagg002@gmail.com>
2024-01-18 19:47:06 -0300

    Controller cadastroUser

--- Controllers/CadastroUserController.cs
+++ Controllers/CadastroUserController.cs
@@ -1,6 +1,25 @@
-﻿namespace PI_Entra21_Back_end.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using PI_Entra21_Back_end.Contracts.Repository;
+using PI_Entra21_Back_end.DTO;
+
+namespace PI_Entra21_Back_end.Controllers
 {
-    public class CadastroUserController
+    [ApiController]
+    [Route("cadastro")]
+    public class CadastroUserController : ControllerBase
     {
+      private readonly ICadastroUserRepository _userRepository;
+        public CadastroUserController(ICadastroUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CadastroUserDTO user)
+        {
+            await _userRepository.Add(user);
+            return Ok();
+        }
+
     }
 }
