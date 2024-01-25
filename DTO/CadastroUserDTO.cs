4b9fc876199bb0a38f8a067feab829810c6a085c
Gustavo Ryan Joaquina <gugagg002@gmail.com>
2024-01-25 12:38:03 -0300

    fix

--- DTO/CadastroUserDTO.cs
+++ DTO/CadastroUserDTO.cs
@@ -3,9 +3,9 @@
     public class CadastroUserDTO
     {
         public string Name { get; set; }
-        public string Age { get; set; }
+        public int Age { get; set; }
         public string Cep { get; set; }
-        public string PhoneNumber { get; set; }
+        public string Phone { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
--- Entity/CadastroUserEntity.cs
+++ Entity/CadastroUserEntity.cs
@@ -4,7 +4,7 @@
     {
 		public int? Id { get; set; }
 		public string Name { get; set; }
-		public string Age { get; set; }
+		public int Age { get; set; }
 		public string Cep { get; set; }
 		public string Phone { get; set; }
 		public string Email { get; set; }
