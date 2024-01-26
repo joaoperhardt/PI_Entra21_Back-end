d07e71cc6b7d3d41ba55becf122fd2ea2fe50bd3
Gustavo Ryan Joaquina <gugagg002@gmail.com>
2024-01-26 12:35:36 -0300

    add validate controller

--- Controllers/CadastroUserController.cs
+++ Controllers/CadastroUserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PI_Entra21_Back_end.Contracts.Repository;
 using PI_Entra21_Back_end.DTO;
+using PI_Entra21_Back_end.Validator;
 
 namespace PI_Entra21_Back_end.Controllers
 {
@@ -15,9 +16,19 @@ namespace PI_Entra21_Back_end.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Add(CadastroUserDTO user)
+        public IActionResult Add(CadastroUserDTO user)
         {
-            await _userRepository.Add(user);
+            var validator = new CadastroValidator();
+
+            var result = validator.Validate(user);
+
+            var error = result.Errors.Select(e => e.ErrorMessage);
+
+            if (!result.IsValid)
+            {
+                return BadRequest(error);
+            }
+
             return Ok();
         }
 
