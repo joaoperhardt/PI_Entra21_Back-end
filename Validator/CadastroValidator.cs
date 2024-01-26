27d4659a9b09d7bb3edba2df8bd3d7714fa0bb1e
Gustavo Ryan Joaquina <gugagg002@gmail.com>
2024-01-26 12:59:30 -0300

    validate phone

--- Validator/CadastroValidator.cs
+++ Validator/CadastroValidator.cs
@@ -7,7 +7,14 @@ namespace PI_Entra21_Back_end.Validator
         public CadastroValidator() 
         {
             RuleFor( c => c.Name ) .NotEmpty().WithMessage("O nome não pode estar vazio") ;
+            
             RuleFor(c => c.Cep).NotEmpty().Length(8).WithMessage("o cep deve conter 8 digitos");
+
+            RuleFor(c => c.Password).NotEmpty().MinimumLength(4).WithMessage("Password deve conter no mínimo 4 caracteres.");
+
+            RuleFor(c => c.Phone).NotEmpty().Length(11).WithMessage("Telefone deve conter exatamente 11 caracteres.");
+
+            RuleFor(c => c.)
         }
     }
 }
